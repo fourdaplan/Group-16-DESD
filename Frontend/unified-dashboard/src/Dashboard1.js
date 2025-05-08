0bade7899122e6c9e3167934f529319ee2674e75
e2-alshehri <eyadalshehri.ssa@gmail.com>
2025-05-08 01:35:54 +0100

    Delete Frontend/unified-dashboard directory

--- Frontend/unified-dashboard/src/Dashboard1.js
+++
@@ -1,8 +0,0 @@
-// Dashboard1.js
-import React from 'react';
-
-const Dashboard1 = () => {
-  return <div>This is Dashboard 1 (Admin)</div>;
-};
-
-export default Dashboard1;
--- Frontend/unified-dashboard/src/Dashboard2.js
+++
@@ -1,8 +0,0 @@
-// Dashboard2.js
-import React from 'react';
-
-const Dashboard2 = () => {
-  return <div>This is Dashboard 2 (End User)</div>;
-};
-
-export default Dashboard2;
