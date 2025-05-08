33ed38c6a1eb263b4893278d460b9a8f4f4cf89c
e2-alshehri <eyadalshehri.ssa@gmail.com>
2025-05-08 18:59:05 +0100

    Update admin.py

--- backend/end_user_panel/admin.py
+++ backend/end_user_panel/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import UploadedFile
 
-admin.site.register(UploadedFile)
+@admin.register(UploadedFile)
+class UploadedFileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'file', 'uploaded_at', 'predicted_settlement', 'cluster', 'feedback')
+    search_fields = ('user__username', 'file', 'feedback')
